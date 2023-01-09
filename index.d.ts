594d921b07b4d951cc77f12a9e08542fd91e9178
TTtie <me@tttie.cz>
2023-01-09 06:56:11 +0000

    add role connection metadata type enum

--- index.d.ts
+++ index.d.ts
@@ -2237,6 +2237,16 @@ declare namespace Dysnomia {
       NITRO_CLASSIC: 1;
       NITRO:         2;
     };
+    RoleConnectionMetadataTypes: {
+      INTEGER_LESS_THAN_OR_EQUAL:     1;
+      INTEGER_GREATER_THAN_OR_EQUAL:  2;
+      INTEGER_EQUAL:                  3;
+      INTEGER_NOT_EQUAL:              4;
+      DATETIME_LESS_THAN_OR_EQUAL:    5;
+      DATETIME_GREATER_THAN_OR_EQUAL: 6;
+      BOOLEAN_EQUAL:                  7;
+      BOOLEAN_NOT_EQUAL:              8;
+    };
     StageInstancePrivacyLevel: {
       PUBLIC: 1;
       GUILD_ONLY: 2;
