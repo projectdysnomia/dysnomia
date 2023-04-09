99038b4231dfa97407dbaeecf5779a169b57cc3e
TTtie <me@tttie.cz>
2023-04-09 16:55:51 +0200

    fixup(Guild): add maxStageVideoChannelUsers to guildUpdate and to toJSON() (#64)

--- index.d.ts
+++ index.d.ts
@@ -585,6 +585,7 @@ declare namespace Dysnomia {
     icon: string | null;
     large: boolean;
     maxMembers?: number;
+    maxStageVideoChannelUsers?: number;
     maxVideoChannelUsers?: number;
     mfaLevel: MFALevel;
     name: string;
