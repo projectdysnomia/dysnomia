18056f70cebe1ea703bafa462535011b6abae818
TTtie <me@tttie.cz>
2023-10-01 14:58:20 +0200

    feat(Client): add `guildScheduledEventID` to `createStageInstance` (#117)

--- index.d.ts
+++ index.d.ts
@@ -1749,6 +1749,7 @@ declare namespace Dysnomia {
 
   // Voice
   interface CreateStageInstanceOptions extends StageInstanceOptions {
+    guildScheduledEventID?: string;
     sendStartNotification?: boolean;
   }
   interface JoinVoiceChannelOptions {
