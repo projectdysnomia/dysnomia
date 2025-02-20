c51aaa1a3d3ec42b3cf0bdadf8e6a6a400de78e1
Snazzah <me@snazzah.com>
2025-02-20 17:14:53 -0600

    feat: support components v2

--- index.d.ts
+++ index.d.ts
@@ -156,7 +156,7 @@ declare namespace Dysnomia {
   type InviteTypes = Constants["InviteTypes"][keyof Constants["InviteTypes"]];
 
   // Message
-  type ActionRowComponents = Button | SelectMenu;
+  type ActionRowComponents = Button | SelectMenu | SectionComponent | TextDisplayComponent | MediaGalleryComponent | SeparatorComponent | FileComponent | ContainerComponent;
   type BaseSelectMenuTypes = Exclude<SelectMenuTypes, SelectMenuExtendedTypes>;
   type Button = InteractionButton | PremiumButton | URLButton;
   type ButtonStyles = Constants["ButtonStyles"][keyof Constants["ButtonStyles"]];
@@ -1501,7 +1501,7 @@ declare namespace Dysnomia {
   }
 
   // Message
-  interface ActionRow {
+  interface ActionRow extends ComponentBase {
     components: ActionRowComponents[];
     type: Constants["ComponentTypes"]["ACTION_ROW"];
   }
@@ -1544,7 +1544,7 @@ declare namespace Dysnomia {
     roles?: boolean | string[];
     users?: boolean | string[];
   }
-  interface ButtonBase {
+  interface ButtonBase extends ComponentBase {
     disabled?: boolean;
     type: Constants["ComponentTypes"]["BUTTON"];
   }
@@ -1552,6 +1552,16 @@ declare namespace Dysnomia {
     channel_types?: GuildChannelTypes[];
     default_values?: SelectMenuDefaultValue<"channel">[];
     type: Constants["ComponentTypes"]["CHANNEL_SELECT"];
+  }
+  interface ContainerComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["CONTAINER"];
+    accent_color?: number;
+    spoiler?: boolean;
+    components: (ActionRow | TextDisplayComponent | SectionComponent | MediaGalleryComponent | SeparatorComponent | FileComponent);
+  }
+  interface ComponentBase {
+    type: ComponentTypes;
+    id?: number;
   }
   interface CreateStickerOptions extends Required<Pick<EditStickerOptions, "name" | "tags" | "description">> {
     file: FileContent;
@@ -1566,6 +1576,20 @@ declare namespace Dysnomia {
     description?: string;
     name?: string;
     tags?: string;
+  }
+  interface FileComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["FILE"];
+    file: UnfurledMediaItem;
+    spoiler?: boolean;
+  }
+  interface MediaGalleryItem {
+    media: UnfurledMediaItem;
+    description?: string;
+    spoiler?: boolean;
+  }
+  interface MediaGalleryComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["MEDIA_GALLERY"];
+    items: MediaGalleryItem[];
   }
   interface MentionableSelectMenu extends SelectMenuBase {
     default_values?: SelectMenuDefaultValue<"any">[];
@@ -1582,7 +1606,7 @@ declare namespace Dysnomia {
   interface BaseSelectMenu extends SelectMenuBase {
     type: BaseSelectMenuTypes;
   }
-  interface SelectMenuBase {
+  interface SelectMenuBase extends ComponentBase {
     custom_id: string;
     disabled?: boolean;
     max_values?: number;
@@ -1593,6 +1617,11 @@ declare namespace Dysnomia {
   interface StringSelectMenu extends SelectMenuBase {
     options: SelectMenuOptions[];
     type: Constants["ComponentTypes"]["STRING_SELECT"];
+  }
+  interface SectionComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["SECTION"];
+    components: TextDisplayComponent[];
+    accessory: ThumbnailComponent | Button;
   }
   interface SelectMenuDefaultValue<Type extends ("any" | "user" | "role" | "channel") = "any"> {
     id: string;
@@ -1605,6 +1634,15 @@ declare namespace Dysnomia {
     label: string;
     value: string;
   }
+  interface SeparatorComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["SEPARATOR"];
+    divider?: boolean;
+    spacing?: Constants["SeparatorSpacingSize"][keyof Constants["SeparatorSpacingSize"]];
+  }
+  interface TextDisplayComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["TEXT_DISPLAY"];
+    content: string;
+  }
   interface TextInput {
     type: Constants["ComponentTypes"]["TEXT_INPUT"];
     custom_id: string;
@@ -1615,6 +1653,12 @@ declare namespace Dysnomia {
     required?: boolean;
     value?: string;
     placeholder?: string;
+  }
+  interface ThumbnailComponent extends ComponentBase {
+    type: Constants["ComponentTypes"]["THUMBNAIL"];
+    media: UnfurledMediaItem;
+    description?: string;
+    spoiler?: boolean;
   }
   interface GetMessageReactionOptions {
     after?: string;
@@ -1718,7 +1762,6 @@ declare namespace Dysnomia {
   interface PremiumButton extends ButtonBase {
     sku_id: string;
     style: Constants["ButtonStyles"]["PREMIUM"];
-
   }
   interface RoleSubscriptionData {
     isRenewal: boolean;
@@ -1749,6 +1792,9 @@ declare namespace Dysnomia {
     name: string;
     sku_id: string;
     stickers: Sticker[];
+  }
+  interface UnfurledMediaItem {
+    url: string;
   }
   interface URLButton extends ButtonBase {
     style: Constants["ButtonStyles"]["LINK"];
@@ -2285,6 +2331,13 @@ declare namespace Dysnomia {
       ROLE_SELECT:        6;
       MENTIONABLE_SELECT: 7;
       CHANNEL_SELECT:     8;
+      SECTION:            9;
+      TEXT_DISPLAY:       10;
+      THUMBNAIL:          11;
+      MEDIA_GALLERY:      12;
+      FILE:               13;
+      SEPARATOR:          14;
+      CONTAINER:          17;
     };
     ConnectionVisibilityTypes: {
       NONE:     0;
@@ -2528,6 +2581,7 @@ declare namespace Dysnomia {
       SUPPRESS_NOTIFICATIONS:                 4096;
       IS_VOICE_MESSAGE:                       8192;
       HAS_SNAPSHOT:                           16384;
+      IS_COMPONENTS_V2:                       32768;
     };
     MessageReferenceTypes: {
       DEFAULT: 0;
@@ -2675,6 +2729,10 @@ declare namespace Dysnomia {
     };
     RoleFlags: {
       IN_PROMPT: 1;
+    };
+    SeparatorSpacingSize: {
+      SMALL: 1;
+      LARGE: 2;
     };
     SKUFlags: {
       AVAILABLE:          4;
